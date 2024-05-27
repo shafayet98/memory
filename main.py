860b79d665d75cde5c75cb819999f374f0c66fe9
shafayet <shafayet.islam.61@gmail.com>
2024-05-27 13:51:35 +0930

    added code for retrieving answer from pdf

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 from langchain_openai import ChatOpenAI
+from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_community.llms import Ollama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.document_loaders import PyPDFLoader
+from langchain.prompts import PromptTemplate
+from langchain_community.vectorstores import DocArrayInMemorySearch
+from dotenv import load_dotenv
+from operator import itemgetter
+from fpdf import FPDF
 
 import os
 from PIL import Image
 import pytesseract
 
+load_dotenv()
+API_KEY = os.getenv("OPENAI_API_KEY")
 
-API_KEY = open("API_KEY", 'r').read()
 
-
-# MODEL = 'gpt-4o'
-MODEL = 'llama3:8b'
+MODEL = 'gpt-4o'
+# MODEL = 'llama3:8b'
 
 if MODEL.startswith('gpt'):
     model = ChatOpenAI(api_key=API_KEY,model = MODEL)
+    embeddings = OpenAIEmbeddings()
 else:
     model = Ollama(model = MODEL)
 
 
-# parser = StrOutputParser()
-# chain = model | parser
+parser = StrOutputParser()
+
 
 # loader = PyPDFLoader('test.pdf')
 # pages = loader.load()
@@ -47,12 +54,64 @@ def extract_text_from_images(folder_path):
                 with Image.open(file_path) as img:
                     # Use pytesseract to extract text
                     text = pytesseract.image_to_string(img)
+                    text2 = text.encode('latin-1', 'replace').decode('latin-1')
                     print(f"Text from {filename}:")
-                    print(text)
-                    print("\n" + "-"*50 + "\n")
+                    # print(text)
+                    # print("\n" + "-"*50 + "\n")
+                    with open('sample.txt', 'a') as file:
+                        file.write(text2)
             except Exception as e:
                 print(f"Failed to process image {filename}: {e}")
 
 # Specify the path to your folder containing images
 folder_path = 'screenshots/'
 extract_text_from_images(folder_path)
+
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("helvetica", size = 12)
+with open('sample.txt', 'r') as file:
+    for line in file:
+        pdf.cell(200, 10, txt = line, ln = True, align = 'L')
+# Save the PDF with name .pdf
+pdf_output = 'sample.pdf'
+pdf.output(pdf_output)
+
+
+
+# load the pdf
+loader = PyPDFLoader("sample.pdf")
+pages = loader.load_and_split()
+
+template = """
+
+Answer the questions based on the context below. The context is full of code snippets. If you cannot, reply with "I don't know"
+
+Context: {context}
+Question: {question}
+
+"""
+
+prompt = PromptTemplate.from_template(template)
+
+vectorstore = DocArrayInMemorySearch.from_documents(
+    pages,
+    embedding= embeddings
+)
+
+retriever = vectorstore.as_retriever()
+# retriever.invoke()
+
+chain = (
+    {
+        "context": itemgetter("question") | retriever,
+        "question": itemgetter("question") 
+    }
+    | prompt
+    | model
+    | parser
+)
+
+res = chain.invoke({"question": "can you find the code snippet about GroundMesh and return that as a reply?"})
+
+print(res)
